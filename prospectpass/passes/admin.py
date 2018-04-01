86f9fdc755fb812393c2b5b8ab566286a08598ec
Yijia Liang <yijial@princeton.edu>
2018-04-01 15:22:26 -0400

    basic user and manager

--- prospectpass/passes/admin.py
+++ prospectpass/passes/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User
+from .models import Student
 from .models import Pass
 
-admin.site.register(User)
+admin.site.register(Student)
 admin.site.register(Pass)
