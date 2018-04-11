6ffa1f544862d3c5d55f032752f71745d33cc0d4
Yijia Liang <yijial@princeton.edu>
2018-04-11 09:30:09 -0400

    minor changes to angular

--- client/public/components/pass/services/student.service.js
+++ client/public/components/pass/services/student.service.js
@@ -1,6 +1,6 @@
 pass.factory('Student', function($resource) {
         return $resource(
-            'http://localhost:8000/students/:id/',
+            'http://localhost:8000/students/',
             {},
             {
                 'query': {
