85eb7d3097c82eb0770fec192615d09b07f6bd22
Yijia Liang <yijial@princeton.edu>
2018-04-11 18:17:23 -0400

    static files

--- prospectpass/prospectpass/settings.py
+++ prospectpass/prospectpass/settings.py
@@ -132,12 +132,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    '/Users/Yijia/Documents/princetonpy/static/',
+    os.path.normpath(os.path.join(BASE_DIR, 'static')),
 )
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
