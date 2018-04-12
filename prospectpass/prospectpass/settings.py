b99990dd9ea8c836774ce5eeaceb83bc6039b31c
cornwellstephen <sjc4@princeton.edu>
2018-04-12 10:09:07 -0400

    minor change for heroku

--- prospectpass/prospectpass/settings.py
+++ prospectpass/prospectpass/settings.py
@@ -15,14 +15,14 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.normpath(os.path.join(BASE_DIR, 'static')),
 )
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
