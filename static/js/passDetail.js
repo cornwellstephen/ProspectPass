cd178fffc83b99475ece59cd7c5c497f58d956d5
oliviajohnsto <ojohnston98@gmail.com>
2018-04-17 11:39:16 -0400

    fixes some design aspects of user homepage.

--- static/js/passDetail.js
+++ static/js/passDetail.js
@@ -8,12 +8,8 @@ function PassDetailController($scope, $attrs, $element) {
         $("#passTransferModal"+this.passNum).modal('hide');
     };
 
-    // $scope.c = Constants.all();
-    // $scope.pass = $scope.m.in[Constants.get("pass")];
-    // $scope.pass_date = django_variables.pass_date;
-    // $scope.club_name = django_variables.club_name;
-    $scope.pass_user = django_variables.pass_user;
-    // $scope.pass_source = django_variables.pass_source;
+    // $scope.pass_user = django_variables.pass_user;
+    // // $scope.pass_source = django_variables.pass_source;
     this.$onInit = function() {
         console.log(this.passObj);
         console.log(this.passNum);
@@ -26,7 +22,8 @@ angular.module('ProspectPassApp').component('passDetail', {
     controller: PassDetailController,
     bindings: {
         passObj: '=',
-        passNum: '<'
+        passNum: '<',
+        passUser: '@'
     }
 });
 
