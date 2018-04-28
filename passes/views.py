02df0e8dfca204396c0e3fa657929ed4a8e01ac7
Yijia Liang <yijial@princeton.edu>
2018-04-28 16:04:16 -0400

    singledist

--- passes/views.py
+++ passes/views.py
@@ -114,12 +114,14 @@ class MultipleFormsDemoView(MultiFormsView):
     form_classes = {'addofficer': AddOfficerForm,
                     'addpass': MakePassForm,
                     'singledist': SingleDist,
+                    'uploadfile' : UploadFileForm,
                     }
 
     success_urls = {
         'addofficer': 'addedofficer',
         'addpass': 'madepass',
         'singledist': 'singledist',
+        'uploadfile' : 'fileuploaded',
     }
 
     def addofficer_form_valid(self, form):
@@ -146,7 +148,39 @@ class MultipleFormsDemoView(MultiFormsView):
 
     def single_dist(self, form):
         pass
+        
+    def uploadfile_form_valid(self, form):
+            csv_file = form.cleaned_data['file']
+            source = form.cleaned_data['source']
+            # if not csv_file.name.endswith('.csv'):
+            #     messages.error(request,'File is not CSV type')
+            #     return HttpResponseRedirect(reverse("myapp:upload_csv"))
+            #if file is too large, return
+            # if csv_file.multiple_chunks():
+            #     messages.error(request,"Uploaded file is too big (%.2f MB)." % (csv_file.size/(1000*1000),))
+            #     return HttpResponseRedirect(reverse("myapp:upload_csv"))
 
+            file_data = csv_file.read().decode("utf-8")     
+
+            lines = file_data.split("\n")
+            source_user = Student.objects.all().filter(NetId=source)[0]
+            source_user.clear_club()
+            i=-1
+            for line in lines:       
+                if i == -1:
+                    j = 0
+                    for entry in line.split(","):
+                        print(i)
+                        if entry.lower() == "netid":
+                            i = j
+                            break
+                        j+=1
+                else:
+                    fields = line.split(",")
+                    source_user = Student.objects.all().filter(NetId=source)[0]
+                    source_user.addToClub(fields[i])
+                    print(fields[i])
+            return HttpResponseRedirect('/fileuploaded')
 # def add_officer(request):
 #     if request.method == 'POST':
 #         form = AddOfficerForm(request.POST, prefix='officer')
